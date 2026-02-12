d698d50cccc9a95f1c639f987ea924726198e3b2
saharshbhansali <saharsh.bhansali@gmail.com>
2026-02-13 02:05:51 +0530

    feat: add error and stdout logs

--- dummy.sh
+++ dummy.sh
@@ -2,6 +2,8 @@
 
 while true; do
   echo "Dummy service is running..." >> /tmp/roadmap-systemd-dummy-service.log
+  echo "Dummy service error log" >&2
+  echo "Dummy service stdout log" >&1
   sleep 10
 done
 
